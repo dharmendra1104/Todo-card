2a6b79ef68f2b569d4a54ccabe6652613d751b3a
dharmendra1104 <dharmendra11042@gmail.com>
2024-02-17 16:48:55 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// let todo=[
+//     {
+//         input:'TO DO',
+//     },
+//     {
+//         input:'IN PROGRESS',
+//     },
+//     {
+//         input:'DONE',
+//     }
+    
+// ]
+
+// let obj=todo.map((item)=>{
+//     return`    <div class="cards">
+//     <nav class="nav">
+//      <div class="head">
+//         <p><i class="fa-solid fa-braille"></i></p>
+//         <input type="text" placeholder="${item.input}">
+//         <p>0</p>
+//      </div>
+//      <p><i class="fa-solid fa-down-left-and-up-right-to-center"></i></p>
+//      <p><i class="fa-solid fa-ellipsis"></i></p>
+//     </nav>
+//    <div class="lower">
+//    <button class="btn"><i class="fa-solid fa-plus"></i></button>
+//     <h6>Create</h6>
+//    </div>
+// </div>`
+// })
+
+// let container=document.querySelector('.main')
+// let add=obj.join('')
+// // console.log(container)
+// container.innerHTML=add
+
+// let button=document.querySelectorAll('.btn')
+
+// button.forEach((item)=>{
+
+//     item.addEventListener('click',(e)=>{
+
+//     })
+// })
+
