5b8126b7052be6ff0e4be367524cd4f6bcf55e23
dharmendra1104 <dharmendra11042@gmail.com>
2024-02-20 19:46:09 +0200

    update

--- index.js
+++ index.js
@@ -1,64 +1,75 @@
-let todo=[
+let todo = [
     {
-        input:'TO DO',
+        input: 'TO DO',
     },
     {
-        input:'IN PROGRESS',
+        input: 'IN PROGRESS',
     },
     {
-        input:'DONE',
+        input: 'DONE',
     }
-    
-]
+];
 
-let obj=todo.map((item)=>{
-    return`
-<div class="main">
-<div class="cards">
-    <nav class="nav">
-     <div class="head">
-        <p><i class="fa-solid fa-braille"></i></p>
-        <input type="text" placeholder="${item.input}">
-        <p>0</p>
-     </div>
-     <p><i class="fa-solid fa-down-left-and-up-right-to-center"></i></p>
-     <p><i class="fa-solid fa-ellipsis"></i></p>
-    </nav>
-   <div class="lower">
-   <button class="btn"><i class="fa-solid fa-plus"></i></button>
-    <h6>Create</h6>
-   </div>
-</div>
-<div class="text-box hide">
-    <input type="text" placeholder="What needs to be done?">
-    <div class="task-card">
-        <div class="task">
-            <span class="icon"><i class="fa-regular fa-square-check"></i></span>
-            <h6>Task</h6>
-            <span><i class="fa-solid fa-angle-down"></i></span>
-        </div>
-        <div class="creat">
-            <h6>Create</h6>
-            <span class="icons"><i class="fa-solid fa-left-long"></i></span>
-        </div>
-    </div>
-</div>
-</div>`
-})
+let centralContainer = document.querySelector('.main-container');
 
-let container=document.querySelector('.main-container')
-let add=obj.join('')
-// console.log(container)
-container.innerHTML=add
+todo.forEach((item) => {
+    let cardContainer = document.createElement('div');
+    cardContainer.className = 'main';
 
-let button=document.querySelectorAll('.lower')
+    let cardHTML = `
+      <div class="cards">
+          <nav class="nav">
+           <div class="head">
+              <p><i class="fa-solid fa-braille"></i></p>
+              <input type="text" placeholder="${item.input}">
+              <p>0</p>
+           </div>
+           <p><i class="fa-solid fa-down-left-and-up-right-to-center"></i></p>
+           <p><i class="fa-solid fa-ellipsis"></i></p>
+          </nav>
+         <div class="lower">
+         <button class="btn"><i class="fa-solid fa-plus"></i></button>
+          <h6>Create</h6>
+         </div>
+      </div>
+      <div class="text-box hide">
+          <input type="text" placeholder="What needs to be done?" class="myinput">
+          <div class="task-card">
+              <div class="task">
+                  <span class="icon"><i class="fa-regular fa-square-check"></i></span>
+                  <h6>Task</h6>
+                  <span><i class="fa-solid fa-angle-down"></i></span>
+              </div>
+              <div class="creat">
+                  <h6>Create</h6>
+                  <span class="icons"><i class="fa-solid fa-left-long"></i></span>
+              </div>
+          </div>
+      </div> `;
 
-button.forEach((item)=>{
+    cardContainer.innerHTML = cardHTML;
+    centralContainer.appendChild(cardContainer);
+    let cards=document.querySelector('.cards')
 
-    item.addEventListener('click',(e)=>{
-         
-        let hide=e.target.closest('.cards').nextElementSibling
-        hide.classList.toggle('hide')
-    })
-})
+    let input = cardContainer.querySelector('.myinput');
+    input.addEventListener('keyup', (e) => {
+        if (e.keyCode === 13) {
+            let value = input.value;
+            let newCard = document.createElement('div');
+            newCard.innerText = value;
+            newCard.className = 'new-card';
+            newCard.style.padding = '10px';
+            newCard.style.borderRadius = '10px';
+            newCard.style.marginTop = '5px';
+            newCard.style.background = 'blue';
 
+            cards.appendChild(newCard);
+        }
+    });
+
+    let button = cardContainer.querySelector('.lower');
+    button.addEventListener('click', (e) => {
+        let hide = e.target.closest('.cards').nextElementSibling;
+        hide.classList.toggle('hide');
+    });
+});
---
+++ ins.js
@@ -0,0 +1,7 @@
+// let input=document.querySelectorAll('.lower')
+// let texbox=document.querySelectorAll('.text-box')
+
+// input.addEventListener('click',()=>{
+//   input.classList.remove('hide')
+//   texbox.classList.add('hide')
+// })
