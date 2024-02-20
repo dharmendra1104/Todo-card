3cae5f697c52db87ef6130beb28dd76be565aa24
dharmendra1104 <dharmendra11042@gmail.com>
2024-02-20 21:34:41 +0200

    update

--- ins.js
+++
@@ -1,7 +0,0 @@
-// let input=document.querySelectorAll('.lower')
-// let texbox=document.querySelectorAll('.text-box')
-
-// input.addEventListener('click',()=>{
-//   input.classList.remove('hide')
-//   texbox.classList.add('hide')
-// })
